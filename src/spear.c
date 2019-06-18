315006ac6a12f9124a3817602e4c67381ed520e7
Ueda Kazuki <ueda.k.ao@m.titech.ac.jp>
2019-06-18 18:28:34 +0900

    completed

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,33 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int i,y;
+    y = 0;
+    for(i = 0; i < n; i++){
+        y = y + A[i]/ x;
+    }
+    return y >= k;
+}
+
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);}
+    lb = 0;
+    ub = 100000;
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        if(p(m)){
+            lb = m;
+        }
+        else {
+            ub = m;
+        }
+    }
+    
+    printf("%d\n",  lb);
+    return 0;
 }
