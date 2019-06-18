e1177c393c2a53e7dfe965d2e411f1c885e6f9bd
Ueda Kazuki <ueda.k.ao@m.titech.ac.jp>
2019-06-18 16:26:19 +0900

    v0.1

--- src/array.c
+++ src/array.c
@@ -8,8 +8,11 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
+    lb = -1
+    ub = n
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(A[m] >= x) ub = m
   }
 
 
