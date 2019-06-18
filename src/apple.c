5fa59e5614de5b6c84841aa5b0890587d763eeef
Ueda Kazuki <ueda.k.ao@m.titech.ac.jp>
2019-06-18 17:51:55 +0900

    completed

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,32 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int i,y;
+    y = 0;
+    for(i = 0; i < n; i++){
+        y = y + (A[i] + x - 1) / x;}
+    return y <= k;
+}
+
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
+      scanf("%d", &A[i]);}
+      lb = 0;
+      ub = 100000;
+      while(ub - lb > 1){
+          int m = (lb + ub) / 2;
+          if(p(m)){
+              ub = m;
+          }
+          else {
+              lb = m;
+          }
+        }
+    
+    printf("%d\n",  ub);
   return 0;
 }
