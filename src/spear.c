c0370370cd3142c7508c77410e7155c413205b2d
Ueda Kazuki <ueda.k.ao@m.titech.ac.jp>
2019-06-21 12:33:43 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -20,7 +20,7 @@ int main(){
     for(i = 0; i < n; i++){
         scanf("%d", &A[i]);}
     lb = 0;
-    ub = 1000000000;
+    ub = 1000000001;
     while(ub - lb > 1){
         int m = (lb + ub) / 2;
         if(p(m)){
